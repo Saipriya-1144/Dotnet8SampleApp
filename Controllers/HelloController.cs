10bc2afe69ff07331c6336aac1173b5e09ba65a7
Saipriya-1144 <saipriyach1144@gmail.com>
2024-09-29 22:20:59 +0530

    modified HelloController.cs

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -36,7 +36,7 @@ namespace DOTNET_8_APP.Controllers
         public string PlaceHolder()
         {
             Console.WriteLine("Placeholder endpoint called!");
-            return "1649ee90-5c57-4d2e-8e5c-e32760fef4c3";
+            return "<<<net-place-holder>>>";
         }
     }
 }
